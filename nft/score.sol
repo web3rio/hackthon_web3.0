33254a86991bbf6a382de6dce6921d217d58603d
hacka crias <web3.cria@gmail.com>
2023-04-02 08:54:02 -0400

    commit final

--- nft/score.sol
+++ nft/score.sol
@@ -2,12 +2,22 @@
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@chainlink/contracts/src/v0.8/Chainlink.sol";
 
 contract ScoreToken is ERC20 {
     mapping (address => uint256) public scores;
-    
-    constructor () ERC20("CustomScore", "SC"){
+    Chainlink private chainlink;
+
+    constructor (address _chainlinkNode, address _chainlinkOracle, string memory _chainlinkJobId) ERC20("CustomScore", "SC"){
         _mint(msg.sender, 1000 * 10 ** decimals());
+        chainlink = Chainlink(_chainlinkOracle);
+        chainlink.setChainlinkToken(address(0x01BE23585060835E02B77ef475b0Cc51aA1e0709)); // Define o endereço do token LINK como padrão
+        chainlink.add(
+            _chainlinkJobId,
+            "https://api.etherscan.io/api?module=account&action=txlist&address=%s&startblock=0&endblock=99999999&sort=asc&apikey=%s&fromBlock=%d&toBlock=%d",
+            "application/json"
+        );
+        chainlink.setChainlinkNode(_chainlinkNode);
     }
 
     function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
@@ -21,4 +31,34 @@ contract ScoreToken is ERC20 {
     function getScore() public view returns (uint256) {
         return scores[msg.sender];
     }
+
+    function checkTransactionCount(address _address) public view returns (bool) {
+        // Define o intervalo de 2 minutos
+        uint256 startDate = block.timestamp - 120;
+        uint256 endDate = block.timestamp;
+
+        // Faz a solicitação HTTP para a API do Etherscan usando o Chainlink
+        Chainlink.Request memory request = chainlink.requestJob(
+            address(this),
+            bytes4(keccak256("fulfillTransactionCount(bytes32,uint256)")),
+            _address,
+            startDate,
+            endDate
+        );
+
+        // Envia a solicitação para o Chainlink
+        bytes32 requestId = chainlink.send(request, 1 ether);
+
+        // Retorna o ID da solicitação
+        return requestId > 0;
+    }
+
+    function fulfillTransactionCount(bytes32 _requestId, uint256 _transactionCount) public {
+        // Verifica se o ID da solicitação é válido e se o número de transações é diferente de zero
+        require(_requestId > 0 && _transactionCount > 0, "Número de transações é igual a zero");
+
+        // Atribui a pontuação ao endereço
+        scores[address(bytes20(_requestId))] += 50;
+    }
 }
+
